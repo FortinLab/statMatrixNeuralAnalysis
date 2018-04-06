ec78fc744b476c4f7875b8c406fb0824c1a5d074
gaelias <gelias@uci.edu>
2018-04-05 19:02:39 -0700

    Repositioning text
    To get positioning right for .pdf output I had to rejigger the position for the spike-phase relationship tables.

--- Analyses/Plotting Functions/PlotUnitFeatures_SM.m
+++ Analyses/Plotting Functions/PlotUnitFeatures_SM.m
@@ -4,7 +4,7 @@ function PlotUnitFeatures_SM(curUnitSummary, curUniSpikeTimes, figID)
 %
 %%
 if nargin==2
-    figure;
+    figID = figure;
 else
     figure(figID);
 end
@@ -82,59 +82,61 @@ text(0,3, {'\bf\fontsize{10}StDev', '(Ang/Circ)'}, 'horizontalalignment', 'left'
 text(0,2, {'\bf\fontsize{10}Vector', 'Length'}, 'horizontalalignment', 'left');
 text(0,1, {'\bf\fontsize{10}R-Test', '(p/r-value)'}, 'horizontalalignment', 'left');
 
-text(3,6, '\bf \fontsize{10} \theta', 'horizontalalignment', 'center');
-text(3,5, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Theta.Mean), 'horizontalalignment', 'center');
-text(3,4, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Theta.Median), 'horizontalalignment', 'center');
-text(3,3, sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.Theta.AngDev,...
-    curUnitSummary.Spike_Phase_Relations.Theta.CircStDev), 'horizontalalignment', 'center');
-text(3,2, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Theta.R_Length), 'horizontalalignment', 'center');
-text(3,1.2, sprintf('r = %.02f', curUnitSummary.Spike_Phase_Relations.Theta.R_Test(2)), 'horizontalalignment', 'center');
-text(3,0.8, sprintf('(p = %.02f)', curUnitSummary.Spike_Phase_Relations.Theta.R_Test(1)), 'horizontalalignment', 'center');
+text(5,6, '\bf \fontsize{9} \theta', 'horizontalalignment', 'center');
+text(5,5, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Theta.Mean)], 'horizontalalignment', 'center');
+text(5,4, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Theta.Median)], 'horizontalalignment', 'center');
+text(5,3, ['\fontsize{8}' sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.Theta.AngDev,...
+    curUnitSummary.Spike_Phase_Relations.Theta.CircStDev)], 'horizontalalignment', 'center');
+text(5,2, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Theta.R_Length)], 'horizontalalignment', 'center');
+text(5,1.2, ['\fontsize{8}' sprintf('r=%.02f', curUnitSummary.Spike_Phase_Relations.Theta.R_Test(2))], 'horizontalalignment', 'center');
+text(5,0.8, ['\fontsize{8}' sprintf('(p=%.02f)', curUnitSummary.Spike_Phase_Relations.Theta.R_Test(1))], 'horizontalalignment', 'center');
 
-text(6,6, '\bf \fontsize{10} Low \beta', 'horizontalalignment', 'center');
-text(6,5, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowBeta.Mean), 'horizontalalignment', 'center');
-text(6,4, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowBeta.Median), 'horizontalalignment', 'center');
-text(6,3, sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.LowBeta.AngDev,...
-    curUnitSummary.Spike_Phase_Relations.LowBeta.CircStDev), 'horizontalalignment', 'center');
-text(6,2, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowBeta.R_Length), 'horizontalalignment', 'center');
-text(6,1.2, sprintf('r = %.02f', curUnitSummary.Spike_Phase_Relations.LowBeta.R_Test(2)), 'horizontalalignment', 'center');
-text(6,0.8, sprintf('(p = %.02f)', curUnitSummary.Spike_Phase_Relations.LowBeta.R_Test(1)), 'horizontalalignment', 'center');
+text(8,6, '\bf \fontsize{9} Low \beta', 'horizontalalignment', 'center');
+text(8,5, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowBeta.Mean)], 'horizontalalignment', 'center');
+text(8,4, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowBeta.Median)], 'horizontalalignment', 'center');
+text(8,3, ['\fontsize{8}' sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.LowBeta.AngDev,...
+    curUnitSummary.Spike_Phase_Relations.LowBeta.CircStDev)], 'horizontalalignment', 'center');
+text(8,2, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowBeta.R_Length)], 'horizontalalignment', 'center');
+text(8,1.2, ['\fontsize{8}' sprintf('r=%.02f', curUnitSummary.Spike_Phase_Relations.LowBeta.R_Test(2))], 'horizontalalignment', 'center');
+text(8,0.8, ['\fontsize{8}' sprintf('(p=%.02f)', curUnitSummary.Spike_Phase_Relations.LowBeta.R_Test(1))], 'horizontalalignment', 'center');
 
-text(9,6, '\bf \fontsize{10} \beta', 'horizontalalignment', 'center');
-text(9,5, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Beta.Mean), 'horizontalalignment', 'center');
-text(9,4, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Beta.Median), 'horizontalalignment', 'center');
-text(9,3, sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.Beta.AngDev,...
-    curUnitSummary.Spike_Phase_Relations.Beta.CircStDev), 'horizontalalignment', 'center');
-text(9,2, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Beta.R_Length), 'horizontalalignment', 'center');
-text(9,1.2, sprintf('r = %.02f', curUnitSummary.Spike_Phase_Relations.Beta.R_Test(2)), 'horizontalalignment', 'center');
-text(9,0.8, sprintf('(p = %.02f)', curUnitSummary.Spike_Phase_Relations.Beta.R_Test(1)), 'horizontalalignment', 'center');
+text(11,6, '\bf \fontsize{9} \beta', 'horizontalalignment', 'center');
+text(11,5, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Beta.Mean)], 'horizontalalignment', 'center');
+text(11,4, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Beta.Median)], 'horizontalalignment', 'center');
+text(11,3, ['\fontsize{8}' sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.Beta.AngDev,...
+    curUnitSummary.Spike_Phase_Relations.Beta.CircStDev)], 'horizontalalignment', 'center');
+text(11,2, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Beta.R_Length)], 'horizontalalignment', 'center');
+text(11,1.2, ['\fontsize{8}' sprintf('r=%.02f', curUnitSummary.Spike_Phase_Relations.Beta.R_Test(2))], 'horizontalalignment', 'center');
+text(11,0.8, ['\fontsize{8}' sprintf('(p=%.02f)', curUnitSummary.Spike_Phase_Relations.Beta.R_Test(1))], 'horizontalalignment', 'center');
 
-text(12,6, '\bf \fontsize{10} Low \gamma', 'horizontalalignment', 'center');
-text(12,5, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowGamma.Mean), 'horizontalalignment', 'center');
-text(12,4, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowGamma.Median), 'horizontalalignment', 'center');
-text(12,3, sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.LowGamma.AngDev,...
-    curUnitSummary.Spike_Phase_Relations.LowGamma.CircStDev), 'horizontalalignment', 'center');
-text(12,2, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowGamma.R_Length), 'horizontalalignment', 'center');
-text(12,1.2, sprintf('r = %.02f', curUnitSummary.Spike_Phase_Relations.LowGamma.R_Test(2)), 'horizontalalignment', 'center');
-text(12,0.8, sprintf('(p = %.02f)', curUnitSummary.Spike_Phase_Relations.LowGamma.R_Test(1)), 'horizontalalignment', 'center');
+text(14,6, '\bf \fontsize{9} Low \gamma', 'horizontalalignment', 'center');
+text(14,5, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowGamma.Mean)], 'horizontalalignment', 'center');
+text(14,4, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowGamma.Median)], 'horizontalalignment', 'center');
+text(14,3, ['\fontsize{8}' sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.LowGamma.AngDev,...
+    curUnitSummary.Spike_Phase_Relations.LowGamma.CircStDev)], 'horizontalalignment', 'center');
+text(14,2, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.LowGamma.R_Length)], 'horizontalalignment', 'center');
+text(14,1.2, ['\fontsize{8}' sprintf('r=%.02f', curUnitSummary.Spike_Phase_Relations.LowGamma.R_Test(2))], 'horizontalalignment', 'center');
+text(14,0.8, ['\fontsize{8}' sprintf('(p=%.02f)', curUnitSummary.Spike_Phase_Relations.LowGamma.R_Test(1))], 'horizontalalignment', 'center');
 
-text(15,6, '\bf \fontsize{10} High \gamma', 'horizontalalignment', 'center');
-text(15,5, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.HighGamma.Mean), 'horizontalalignment', 'center');
-text(15,4, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.HighGamma.Median), 'horizontalalignment', 'center');
-text(15,3, sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.HighGamma.AngDev,...
-    curUnitSummary.Spike_Phase_Relations.HighGamma.CircStDev), 'horizontalalignment', 'center');
-text(15,2, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.HighGamma.R_Length), 'horizontalalignment', 'center');
-text(15,1.2, sprintf('r = %.02f', curUnitSummary.Spike_Phase_Relations.HighGamma.R_Test(2)), 'horizontalalignment', 'center');
-text(15,0.8, sprintf('(p = %.02f)', curUnitSummary.Spike_Phase_Relations.HighGamma.R_Test(1)), 'horizontalalignment', 'center');
+text(17,6, '\bf \fontsize{9} High \gamma', 'horizontalalignment', 'center');
+text(17,5, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.HighGamma.Mean)], 'horizontalalignment', 'center');
+text(17,4, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.HighGamma.Median)], 'horizontalalignment', 'center');
+text(17,3, ['\fontsize{8}' sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.HighGamma.AngDev,...
+    curUnitSummary.Spike_Phase_Relations.HighGamma.CircStDev)], 'horizontalalignment', 'center');
+text(17,2, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.HighGamma.R_Length)], 'horizontalalignment', 'center');
+text(17,1.2, ['\fontsize{8}' sprintf('r=%.02f', curUnitSummary.Spike_Phase_Relations.HighGamma.R_Test(2))], 'horizontalalignment', 'center');
+text(17,0.8, ['\fontsize{8}' sprintf('(p=%.02f)', curUnitSummary.Spike_Phase_Relations.HighGamma.R_Test(1))], 'horizontalalignment', 'center');
 
-text(18,6, '\bf \fontsize{10} Ripple', 'horizontalalignment', 'center');
-text(18,5, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Ripple.Mean), 'horizontalalignment', 'center');
-text(18,4, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Ripple.Median), 'horizontalalignment', 'center');
-text(18,3, sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.Ripple.AngDev,...
-    curUnitSummary.Spike_Phase_Relations.Ripple.CircStDev), 'horizontalalignment', 'center');
-text(18,2, sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Ripple.R_Length), 'horizontalalignment', 'center');
-text(18,1.2, sprintf('r = %.02f', curUnitSummary.Spike_Phase_Relations.Ripple.R_Test(2)), 'horizontalalignment', 'center');
-text(18,0.8, sprintf('(p = %.02f)', curUnitSummary.Spike_Phase_Relations.Ripple.R_Test(1)), 'horizontalalignment', 'center');
+text(20,6, '\bf \fontsize{9} Ripple', 'horizontalalignment', 'center');
+text(20,5, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Ripple.Mean)], 'horizontalalignment', 'center');
+text(20,4, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Ripple.Median)], 'horizontalalignment', 'center');
+text(20,3, ['\fontsize{8}' sprintf('%.02g (%.02g)', curUnitSummary.Spike_Phase_Relations.Ripple.AngDev,...
+    curUnitSummary.Spike_Phase_Relations.Ripple.CircStDev)], 'horizontalalignment', 'center');
+text(20,2, ['\fontsize{8}' sprintf('%.02g', curUnitSummary.Spike_Phase_Relations.Ripple.R_Length)], 'horizontalalignment', 'center');
+text(20,1.2, ['\fontsize{8}' sprintf('r=%.02f', curUnitSummary.Spike_Phase_Relations.Ripple.R_Test(2))], 'horizontalalignment', 'center');
+text(20,0.8, ['\fontsize{8}' sprintf('(p=%.02f)', curUnitSummary.Spike_Phase_Relations.Ripple.R_Test(1))], 'horizontalalignment', 'center');
 
 title({'Session Wide','Spike Phase Relationship'})
 
+set(figID, 'PaperOrientation', 'landscape');
+print('-fillpage', figID, '-dpdf', [curUnitSummary.UnitName '_Unit_Feature_Summary.pdf']);
