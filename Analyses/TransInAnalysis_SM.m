d244d011b072dd713548b25509d5446ad885ac1a
gaelias <gelias@uci.edu>
2018-04-17 17:41:35 -0700

    Tweaks & Additions
    Added in a few plots at the bottom, tweaked the windows and bins a bit.
    
    This file will probably be chopped up into different files, much of it has already been transitioned over to FratioAnalysis2_SM.m

--- Analyses/TransInAnalysis_SM.m
+++ Analyses/TransInAnalysis_SM.m
@@ -7,7 +7,9 @@ close all
 clear all
 %%
 pehBinSize = 0.125;
-eventWindow = [-0.8 0.5];
+eventWindow = [-0.75 0.5];
+fHistMtxBins = 0:0.25:5.25;
+fHistDiffMtxBins = -5.25:0.25:5.25;
 %%
 origDir = cd;
 [fileDir] = uigetdir(origDir);
@@ -26,6 +28,7 @@ load(fileNames{cellfun(@(a)~isempty(a), strfind(fileNames, 'EnsembleMatrix'))});
 
 %%
 pokeInAlignedBehavMatrix = OrganizeTrialData_SM(behavMatrix, behavMatrixColIDs, eventWindow, 'PokeIn');
+% pokeInAlignedBehavMatrix = OrganizeTrialData_SM(behavMatrix, behavMatrixColIDs, eventWindow, 'PokeOut');
 
 outSeqLog = ~([pokeInAlignedBehavMatrix.TranspositionDistance]==0);
 trialPerfLog = [pokeInAlignedBehavMatrix.Performance]==1;
@@ -45,13 +48,13 @@ posIDs = transInTrlsPos;
 
 %%
 pokeInAlignedBehavMatrix = OrganizeTrialData_SM(behavMatrix, behavMatrixColIDs, eventWindow, 'PokeIn');
+% pokeInAlignedBehavMatrix = OrganizeTrialData_SM(behavMatrix, behavMatrixColIDs, eventWindow, 'PokeOut');
 
-trialPerfLog = [pokeInAlignedBehavMatrix.Performance]==0;
+trialPerfLog = [pokeInAlignedBehavMatrix.Performance]==1;
 firstPosLog = [pokeInAlignedBehavMatrix.Position]==1;
 
 
 goodTrials = find(trialPerfLog&~firstPosLog);
-% goodTrials = find(trialPerfLog);
 odorIDs = [pokeInAlignedBehavMatrix(goodTrials).Odor];
 posIDs = [pokeInAlignedBehavMatrix(goodTrials).Position];
 
@@ -81,7 +84,7 @@ for u = 2:length(ensembleMatrixColIDs)
             elseif curOdorF>5
                 odorF(ndx) = 5;
             else
-                odorF(ndx) = 0;
+                odorF(ndx) = nan;
             end
             [~,posTable,~] = anova1(curUniUnpacked(:,ndx), posIDs', 'off');
             curPosF = posTable{2,5};
@@ -90,7 +93,7 @@ for u = 2:length(ensembleMatrixColIDs)
             elseif curPosF>5
                 positionF(ndx) = 5;
             else
-                positionF(ndx) = 0;
+                positionF(ndx) = nan;
             end
         end
     end
@@ -100,7 +103,8 @@ for u = 2:length(ensembleMatrixColIDs)
 end
 
 fRatDiffMtx = cell2mat(fRatDiff);
-fPosValMtx = cell2mat(fPosVal);fOdrValMtx = cell2mat(fOdrVal);
+fPosValMtx = cell2mat(fPosVal);
+fOdrValMtx = cell2mat(fOdrVal);
 
 transInMean = nanmean(fRatDiffMtx);
 transInPosMean = nanmean(fPosValMtx);
@@ -110,22 +114,62 @@ transInSEM = nanstd(fRatDiffMtx,0,1)./(sum(~isnan(fRatDiffMtx))-1);
 transInPosSEM = nanstd(fPosValMtx,0,1)./(sum(~isnan(fPosValMtx))-1);
 transInOdrSEM = nanstd(fOdrValMtx,0,1)./(sum(~isnan(fOdrValMtx))-1);
 
+diffFhistMtx = nan(length(fHistDiffMtxBins)-1, size(fRatDiffMtx,2));
+posFhistMtx = nan(length(fHistMtxBins)-1, size(fPosValMtx,2));
+odrFhistMtx = nan(length(fHistMtxBins)-1, size(fOdrValMtx,2));
+for ndx = 1:size(fPosValMtx,2)
+    diffFhistMtx(:,ndx) = histcounts(fRatDiffMtx(:,ndx), fHistDiffMtxBins);
+    posFhistMtx(:,ndx) = histcounts(fPosValMtx(:,ndx), fHistMtxBins);
+    odrFhistMtx(:,ndx) = histcounts(fOdrValMtx(:,ndx), fHistMtxBins);
+end
 
 xTicks = (eventWindow(1):pehBinSize:(eventWindow(2)-pehBinSize))+(pehBinSize/2);
 
 figure
+subplot(2,2,1)
 PlotLineAndFilledError(xTicks, transInMean, transInSEM, 'blue');
+hold on;
+line([0 0], get(gca, 'ylim'), 'color', 'black', 'linestyle', ':', 'linewidth', 1.5);
+line(get(gca, 'xlim'), [0 0], 'color', 'black', 'linestyle', ':', 'linewidth', 1.5);
+subplot(2,2,2)
+imagesc(xTicks, fHistDiffMtxBins, flipud(diffFhistMtx));
+subplot(2,2,3)
+histogram(fRatDiffMtx(:), fHistDiffMtxBins);
 
 figure
 pos = PlotLineAndFilledError(xTicks, transInPosMean, transInPosSEM, 'red');
 hold on;
 odr = PlotLineAndFilledError(xTicks, transInOdrMean, transInOdrSEM, 'blue');
-line([0 0], get(gca, 'ylim'), 'color', 'black', 'linestyle',':', 'linewidth', 1.5);
+line([0 0], get(gca, 'ylim'), 'color', 'black', 'linestyle', ':', 'linewidth', 1.5);
+line(get(gca, 'xlim'), [1 1], 'color', 'black', 'linestyle', ':', 'linewidth', 1.5);
 legend([pos odr], 'Position', 'Odor');
 axis tight
 title('Poke In Aligned F-Ratios');
 xlabel('Time Relative to Poke Initiation');
 ylabel('F-Ratio');
+
+    
+figure
+posHist = subplot(2,2,1);
+histogram(fPosValMtx(:), fHistMtxBins)
+title('Position');
+posHistMtx = subplot(2,2,2);
+imagesc(xTicks, fHistMtxBins, flipud(posFhistMtx));
+set(gca, 'ytick', fHistMtxBins(1:4:length(fHistMtxBins)), 'yticklabel', fHistMtxBins(length(fHistMtxBins):-4:1));
+cLims1 = get(gca, 'clim');
+odrHist = subplot(2,2,3);
+histogram(fOdrValMtx(:), fHistMtxBins)
+title('Odor');
+odrHistMtx = subplot(2,2,4);
+imagesc(xTicks, fHistMtxBins, flipud(odrFhistMtx));
+set(gca, 'ytick', fHistMtxBins(1:4:length(fHistMtxBins)), 'yticklabel', fHistMtxBins(length(fHistMtxBins):-4:1));
+linkaxes([posHist odrHist], 'xy');
+cLims2 = get(gca, 'clim');
+cLimMax = max([cLims1 cLims2]);
+set(posHistMtx, 'clim', [0 cLimMax]);
+set(odrHistMtx, 'clim', [0 cLimMax]);
+% colormap jet
+
 
 
 
