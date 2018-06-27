0acde561ea2e2690d8aad4006f72f2647749e7c2
gaelias <gelias@uci.edu>
2018-06-27 13:36:05 -0700

    Rebin PEH2 update
    Reworked to enable identification of window size based on sample rate which was added as an additional input

--- Data Management/RebinPEH2_SM.m
+++ Data Management/RebinPEH2_SM.m
@@ -1,4 +1,5 @@
-function [binnedData, newBins] = RebinPEH2_SM(trialOrgSpikes, window, newBinSize)
+function [binnedData, newBins] = RebinPEH2_SM(trialOrgSpikes, window, newBinSize, sampleRate)
+
 
 if iscell(trialOrgSpikes)
     spkLog = logical(cell2mat(trialOrgSpikes)');
@@ -8,8 +9,21 @@ else
     spkLog = logical(trialOrgSpikes)';
 end
 
-newBins = window(1):newBinSize:window(2);
-oldBins = linspace(window(1), window(2), size(spkLog,2));
+if ~isempty(window)
+    newBins = window(1):newBinSize:window(2);
+    oldBins = linspace(window(1), window(2), size(spkLog,2));
+else
+    if nargin==3 || isempty(sampleRate)
+        error('If not specifying a time period for the window, you must specify a samplerate at which the data is collected/binned');
+    else
+        windowSize = (size(spkLog,2)*1)/sampleRate;
+        if windowSize<newBinSize
+            newBinSize = windowSize-(1/sampleRate);
+        end
+        newBins = 0:newBinSize:windowSize;
+        oldBins = (0:size(spkLog,2)-1)/sampleRate;
+    end
+end
 binnedData = nan(size(spkLog,1), length(newBins)-1);
 for row = 1:size(spkLog,1)
     oldSpkRelTimes = oldBins(spkLog(row,:));
