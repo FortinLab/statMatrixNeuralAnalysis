765a5f3c0c4a06b9cf89655e6e0a59cb5f372246
gaelias <gelias@uci.edu>
2018-07-01 15:28:27 -0700

    Update to function on combined files... in progress
    A few things here...
    -Included code to account for position data that is a combination of multiple LEDs
    -Position timestamps also needed to have the pre-timestamp(1) removed... not sure why/how they're there but it was erroring things out. This is what led to the other change below.
    -Modified the timestamp vector so that timestamp reflects the beginning of the time bin, not the end like it was before.
    - Modified the behavior column identifications to be based off of #Odors + #Positions rather than just #Odors*2
    - Updated the channel checks to account for tetrodes named 'T1" or 'T01' as both are used in different files.
    - Also added in additional command line comments to indicate where in the code it currently is.
    
    To do:
    - Add in columns that indicate pre-trial/trial/post-trial periods... should be able to accomplish this using the fragment boundaries/lengths
    - Make a file output that summarizes the running of the script.

--- statMatrix Creation/CreateStatMatrixFromPLX2.m
+++ statMatrix Creation/CreateStatMatrixFromPLX2.m
@@ -18,6 +18,13 @@ if nargin == 0
 end
 outputFileName = inputdlg('Name Output File', 'Filename', 1, {fileName});
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% Add in something that enables creation of logical vectors for
+%%%%% different periods of the recording... i.e. pre-Session, Session and
+%%%%% post-Session recording periods... probably some kind of input system
+%%%%% similar to the code to combine ssnData files but operating on the plx
+%%%%% files.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Behavioral Data
 % [behaviorData] = SummarizePLXabbr_BOS(plxFile);
 [behaviorData] = SummarizePLXevents_SD(plxFile);
@@ -35,10 +42,11 @@ tsVect = ((0:(fn(1)-1))/samp)+tetTS(1);
 for fragNum = 2:length(tetTS)
     tsVect = [tsVect, ((0:(fn(fragNum)-1))/samp)+tetTS(fragNum)]; %#ok<AGROW>
 end
+tsVect(end+1) = tsVect(end)+(1/samp);
 ssnData = behaviorData.Raw;
 seqLength = behaviorData.Summary.SequenceLength;
 maxSeqLength = max([ssnData.OrdinalPosition]);
-behVals = nan(length(tsVect), seqLength + maxSeqLength + 5);
+behVals = nan(length(tsVect)-1, seqLength + maxSeqLength + 5);
 behDataHeaders = cell(1,seqLength + maxSeqLength + 7);
 % Step through each sequence item/position and identify when odors were
 % presented
@@ -46,36 +54,38 @@ for seq = 1:seqLength
     for pos = 1:maxSeqLength
         itemPresTimes = [ssnData([ssnData.SequenceItem]==seq).ItemPresentationTime];
         posPresTimes = [ssnData([ssnData.OrdinalPosition]==pos).ItemPresentationTime];
-        behVals(:,seq) = histcounts(itemPresTimes, [0 tsVect])';
-        behVals(:,pos+seqLength) = histcounts(posPresTimes, [0 tsVect])';
+        behVals(:,seq) = histcounts(itemPresTimes, tsVect)';
+        behVals(:,pos+seqLength) = histcounts(posPresTimes, tsVect)';
         behDataHeaders{seq} = ['Odor' num2str(seq)];
         behDataHeaders{pos+seqLength} = ['Position' num2str(pos)];
     end
 end
 inSeqOdorPres = [ssnData([ssnData.TranspositionDistance]==0).ItemPresentationTime];
 outSeqOdorPres = [ssnData(~([ssnData.TranspositionDistance]==0)).ItemPresentationTime];
-behVals(:,seqLength+maxSeqLength+1) = histcounts(inSeqOdorPres, [0 tsVect])' - histcounts(outSeqOdorPres,[0 tsVect])';
-behDataHeaders{seq+seqLength+1} = 'InSeqLog';
-
+behVals(:,seqLength+maxSeqLength+1) = histcounts(inSeqOdorPres, tsVect)' - histcounts(outSeqOdorPres,tsVect)';
+behDataHeaders{maxSeqLength+seqLength+1} = 'InSeqLog';
 itmPresTimes = [ssnData.ItemPresentationTime];
 trialPerformance = [ssnData.Performance];
 corrTrials = itmPresTimes(logical(trialPerformance));
-corTrlHistCounts = histcounts(corrTrials, [0 tsVect])';
+corTrlHistCounts = histcounts(corrTrials, tsVect)';
 inCorrTrials = itmPresTimes(~logical(trialPerformance));
-inCorTrlHistCounts = histcounts(inCorrTrials, [0 tsVect])';
-behVals(:,seqLength+seqLength+2) = corTrlHistCounts + (inCorTrlHistCounts*-1);
-behDataHeaders{seq+seqLength+2} = 'PerformanceLog';
+inCorTrlHistCounts = histcounts(inCorrTrials, tsVect)';
+behVals(:,seqLength + maxSeqLength+2) = corTrlHistCounts + (inCorTrlHistCounts*-1);
+behDataHeaders{seqLength + maxSeqLength+2} = 'PerformanceLog';
 
-behVals(:,seqLength+seqLength+3) = histcounts([ssnData.OdorTrigPokeTime], [0 tsVect])';
-behVals(:,seqLength+seqLength+3) = behVals(:,seqLength+seqLength+3) - histcounts([ssnData.OdorPokeWithdrawTime], [0 tsVect])';
-behDataHeaders{seq+seqLength+3} = 'PokeEvents';
+behVals(:,seqLength + maxSeqLength+3) = histcounts([ssnData.OdorTrigPokeTime], tsVect)' - histcounts([ssnData.OdorPokeWithdrawTime], tsVect)';
+behDataHeaders{seqLength + maxSeqLength+3} = 'PokeEvents';
 
-behVals(:,seqLength+seqLength+4) = histcounts([ssnData.FrontRewardTime], [0 tsVect])';
-behDataHeaders{seq+seqLength+4} = 'FrontReward';
+behVals(:,seqLength + maxSeqLength+4) = histcounts([ssnData.FrontRewardTime], tsVect)';
+behDataHeaders{seqLength + maxSeqLength+4} = 'FrontReward';
 
-behVals(:,seqLength+seqLength+5) = histcounts([ssnData.BackRewardTime], [0 tsVect])';
-behDataHeaders{seq+seqLength+5} = 'BackReward';
+behVals(:,seqLength + maxSeqLength+5) = histcounts([ssnData.BackRewardTime], tsVect)';
+behDataHeaders{seqLength + maxSeqLength+5} = 'BackReward';
 
+if isfield(behaviorData.Raw, 'ErrorSignalTime')
+    behVals(:,seqLength + maxSeqLength+6) = histcounts([ssnData.ErrorSignalTime], tsVect)';
+    behDataHeaders{seqLength + maxSeqLength+6} = 'ErrorSignal';
+end
 
 [numChans, chanNames] = plx_event_names(plxFile);
 findingStrobed = 1;
@@ -88,20 +98,53 @@ while findingStrobed
         if strobedChanLog
             [~, strobedTS, strobedSV] = plx_event_ts(plxFile, curChan);
             [~, ~, ~, aniPosition] = plx_vt_interpret(strobedTS, strobedSV);
-            aniPosHistBins = find(histcounts(aniPosition(:,1), [0 tsVect]));
-            behVals(aniPosHistBins,seqLength+seqLength+6) = aniPosition(:,2);
-            behVals(aniPosHistBins,seqLength+seqLength+7) = aniPosition(:,3);
+            aniPosition(aniPosition(:,1)<tsVect(1),:) = [];
+            aniX = aniPosition(:,2);
+            aniY = aniPosition(:,3);
+            if size(aniPosition,2)>=5
+                for t = 1:size(aniPosition,1)
+                    if (aniPosition(t,2)==0 && aniPosition(t,3)==0) &&...
+                            (aniPosition(t,4)>0 && aniPosition(t,5)>0)
+                        aniX(t) = aniPosition(t,4);
+                        aniY(t) = aniPosition(t,5);
+                    elseif (aniPosition(t,4)==0 && aniPosition(t,5)==0) &&...
+                            (aniPosition(t,2)>0 && aniPosition(t,3)>0)
+                        aniX(t) = aniPosition(t,2);
+                        aniY(t) = aniPosition(t,3);
+                    elseif (aniPosition(t,2)>0 && aniPosition(t,3)>0) &&...
+                            (aniPosition(t,3)>0 && aniPosition(t,5)>0)
+                        aniX(t) = mean([aniPosition(t,2) aniPosition(t,4)]);
+                        aniY(t) = mean([aniPosition(t,3) aniPosition(t,5)]);
+                    elseif (aniPosition(t,2)==0 && aniPosition(t,3)==0) &&...
+                            (aniPosition(t,3)==0 && aniPosition(t,5)==0)
+                        aniX(t) = 0;
+                        aniY(t) = 0;
+                    end
+                end
+            end
+            aniPosHistBins = find(histcounts(aniPosition(:,1), tsVect));
+            if isfield(behaviorData.Raw, 'ErrorSignalTime')
+                behVals(aniPosHistBins,seqLength + maxSeqLength+7) = aniX';
+                behVals(aniPosHistBins,seqLength + maxSeqLength+8) = aniY';
+            else
+                behVals(aniPosHistBins,seqLength + maxSeqLength+6) = aniX;
+                behVals(aniPosHistBins,seqLength + maxSeqLength+7) = aniY;
+            end
             findingStrobed = 0;
         end
     end
 end
-behDataHeaders{seq+seqLength+6} = 'XvalRatMazePosition';
-behDataHeaders{seq+seqLength+7} = 'YvalRatMazePosition';
-behavMatrix = behVals; %#ok<NASGU>
-behavMatrixColIDs = behDataHeaders; %#ok<NASGU>
+if isfield(behaviorData.Raw, 'ErrorSignalTime')
+    behDataHeaders{seqLength + maxSeqLength+7} = 'XvalRatMazePosition';
+    behDataHeaders{seqLength + maxSeqLength+8} = 'YvalRatMazePosition';
+else
+    behDataHeaders{seqLength + maxSeqLength+6} = 'XvalRatMazePosition';
+    behDataHeaders{seqLength + maxSeqLength+7} = 'YvalRatMazePosition';
+end
+behavMatrix = [tsVect(1:end-1)', behVals]; %#ok<NASGU>
+behavMatrixColIDs = [{'TimeBin'}, behDataHeaders]; %#ok<NASGU>
 save([filePath outputFileName{1} '_BehaviorMatrix.mat'], 'behavMatrix', 'behavMatrixColIDs');
 disp('Behavior data saved.');
-
 %% Neural Data
 [tsCountFl, ~, ~, contCountFl] = plx_info(plxFile, 1);
 [~, plxADchanNames] = plx_adchan_names(plxFile);
@@ -110,26 +153,36 @@ for chan = 2:4:size(tsCountFl,2)
     if sum((chan-1)==tetChans)==1
         curNumUnis = sum(~tsCountFl(2:end, chan)==0);
         tetNum = find(tetChans==(chan-1));
-        tetName = ['T' num2str(tetNum)];
+        tetName1 = sprintf('T%i', tetNum);
+        tetName2 = sprintf('T%02i', tetNum);
         r = 0;
         adChan = [];
         while r < size(plxADchanNames,1)
             r = r + 1;
-            chanCheck1a = ~isempty(strfind(plxADchanNames(r,:), [tetName '-1']));
-            chanCheck1b = ~isempty(strfind(plxADchanNames(r,:), [tetName '_1']));
-            chanCheck2 = ~isempty(strfind(plxADchanNames(r,:), [tetName '-3']));
-            chanCheck2b = ~isempty(strfind(plxADchanNames(r,:), [tetName '_3']));
+            chanCheck1a1 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s-1', tetName1)));
+            chanCheck1a2 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s-1', tetName2)));
+            chanCheck1b1 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s_1', tetName1)));
+            chanCheck1b2 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s_1', tetName2)));
+            chanCheck2a1 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s-3', tetName1)));
+            chanCheck2a2 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s-3', tetName2)));
+            chanCheck2b1 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s_3', tetName1)));
+            chanCheck2b2 = ~isempty(strfind(plxADchanNames(r,:), sprintf('%s_3', tetName2)));
             if contCountFl(r)>0
-                if chanCheck1a
+                if chanCheck1a1 || chanCheck1b1 || chanCheck2a1 || chanCheck2b1
+                    tetName = tetName1;
+                elseif chanCheck1a2 || chanCheck1b2 || chanCheck2a2 || chanCheck2b2
+                    tetName = tetName2;
+                end
+                if chanCheck1a1 || chanCheck1a2
                     adChan = [tetName '-1'];
                     break
-                elseif chanCheck1b
+                elseif chanCheck1b1 || chanCheck1b2
                     adChan = [tetName '_1'];
                     break
-                elseif chanCheck2
+                elseif chanCheck2a1 || chanCheck2a2
                     adChan = [tetName '-3'];
                     break
-                elseif chanCheck2b
+                elseif chanCheck2b1 || chanCheck2b2
                     adChan = [tetName '_3'];
                     break
                 end
@@ -137,26 +190,29 @@ for chan = 2:4:size(tsCountFl,2)
         end
         
         if ~isempty(adChan)
+            fprintf('Found LFP data for %s on %s...........', tetName, adChan);
             [samp, ~, tetTS, tetFN, tetV] = plx_ad_v(plxFile, adChan);
             tsVect = ((0:(tetFN(1)-1))/samp)+tetTS(1);
             for fragNum = 2:length(tetTS)
                 tsVect = [tsVect, ((0:(tetFN(fragNum)-1))/samp)+tetTS(fragNum)]; %#ok<AGROW>
             end
-            [tetRawHilb, tetRawLFP] = PhaseFreqDetectAbbr(tetV,tsVect);
-            [tetThetaHilb, tetThetaLFP] = PhaseFreqDetectAbbr(tetV,tsVect,4,12);
-            [tetLowBetaHilb, tetLowBetaLFP] = PhaseFreqDetectAbbr(tetV,tsVect,13,19);
-            [tetBetaHilb, tetBetaLFP] = PhaseFreqDetectAbbr(tetV,tsVect,20,40);
-            [tetLowGammaHilb, tetLowGammaLFP] = PhaseFreqDetectAbbr(tetV,tsVect,41,59);
-            [tetHighGammaHilb, tetHighGammaLFP] = PhaseFreqDetectAbbr(tetV,tsVect,61,80);
-            [tetRipHilb, tetRipLFP] = PhaseFreqDetectAbbr(tetV,tsVect,150,250);
+            tsVect(end+1) = tsVect(end)+(1/samp); %#ok<AGROW>
+
+            [tetRawHilb, tetRawLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1));
+            [tetThetaHilb, tetThetaLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1),4,12);
+            [tetLowBetaHilb, tetLowBetaLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1),13,19);
+            [tetBetaHilb, tetBetaLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1),20,40);
+            [tetLowGammaHilb, tetLowGammaLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1),41,59);
+            [tetHighGammaHilb, tetHighGammaLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1),61,80);
+            [tetRipHilb, tetRipLFP] = PhaseFreqDetectAbbr(tetV,tsVect(1:end-1),150,250);
             tetUnitTSs = cell(1,curNumUnis);
             tetUnitIDs = cell(1,curNumUnis);
             for uni = 1:curNumUnis
                 [~, tempTSs] = plx_ts(plxFile, chan-1, uni);
-                tetUnitTSs{uni} = histcounts(tempTSs, [0 tsVect])';
+                tetUnitTSs{uni} = histcounts(tempTSs, tsVect)';
                 tetUnitIDs{uni} = [tetName '-U' num2str(uni)];
             end
-            statMatrix = [tsVect',tetRawLFP, tetRawHilb,...
+            statMatrix = [tsVect(1:end-1)',tetRawLFP, tetRawHilb,...
                 tetThetaLFP, tetThetaHilb,...
                 tetLowBetaLFP, tetLowBetaHilb,...
                 tetBetaLFP, tetBetaHilb,...
@@ -173,9 +229,9 @@ for chan = 2:4:size(tsCountFl,2)
                 {[tetName '_LFP_Ripple']}, {[tetName '_LFP_Ripple_HilbVals']},...
                 tetUnitIDs, behDataHeaders]; %#ok<NASGU>
             save([filePath outputFileName{1} '_' tetName '.mat'], 'statMatrix', 'statMatrixColIDs');
-            disp([tetName ' saved!']);
+            fprintf('%s saved!\n', tetName);
         else
-            disp(['No ADchan identified for ' tetName]);
+            fprintf('No AD chan found for %s or %s\n', tetName1, tetName2);
         end
     end
 end
