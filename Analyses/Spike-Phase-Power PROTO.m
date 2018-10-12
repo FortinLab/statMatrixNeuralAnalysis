fd6b06749cddd701bd0be6812804067d5c9a910b
gabe <g.a.elias@gmail.com>
2018-10-12 15:50:09 -0700

    Update Analyses/Spike-Phase-Power PROTO.m
    Added in normalization by time each band showed phase/power combination

--- Analyses/Spike-Phase-Power PROTO.m
+++ Analyses/Spike-Phase-Power PROTO.m
@@ -61,29 +61,51 @@ for tet = 1:length(tetFiles)
     else
         hilbTrace = cell(1,length(bands));
         rmsTrace = cell(1,length(bands));
+        phasePowerNorm = cell(1,length(bands));
         for bnd = 1:length(bands)
             windowNdxs = windowSize.(bands{bnd})*sampleRate;
-            hlfWndo = windowNdxs/2;
             voltColLog = cellfun(@(a)~isempty(a), regexp(statMatrixColIDs,{[sprintf('_%s',bands{bnd}) '$']}));
             hilbColLog = cellfun(@(a)~isempty(a), regexp(statMatrixColIDs,{[sprintf('_%s',bands{bnd}) '_Hilb']}));
             hilbTrace{bnd} = statMatrix(:,hilbColLog);
             rmsTrace{bnd} = zscore(conv(sqrt(conv(statMatrix(:,voltColLog).^2, ones(windowNdxs,1)/windowNdxs, 'same')), w, 'same'));
+            [phasePowerNorm{bnd},~,~] = histcounts2(hilbTrace{bnd}, rmsTrace{bnd}, linspace(-pi,pi,13),0:0.5:10);
         end
         uniSpots = find(uniColLog);
         for u = 1:numUnis
             curUniSpkLog = statMatrix(:,uniSpots(u))>0;
             figure('Name', statMatrixColIDs{uniSpots(u)});
             annotation('textbox', [0.05 0.9 0.9 0.1], 'String', sprintf('%s', statMatrixColIDs{uniSpots(u)}), 'linestyle', 'none', 'interpreter', 'none');
+            sps = nan(1,length(bands));
+            spsMax = nan(1,length(bands));
+            spsNorm = nan(1,length(bands));
+            spsNormMax = nan(1,length(bands));
             for bnd = 1:length(bands)
                 curUniPhasePref = hilbTrace{bnd}(curUniSpkLog);
                 curUniPowerPref = rmsTrace{bnd}(curUniSpkLog);
                 [sppPlot,Xedges,Yedges] = histcounts2(curUniPhasePref,curUniPowerPref,linspace(-pi,pi,13),0:0.5:10);
-                subplot(1,length(bands),bnd)
+                sps(bnd) = subplot(2,length(bands),bnd);
                 imagesc(-pi:0.1:(pi-0.1), 0:0.5:9.5, flipud(sppPlot'));
                 set(gca, 'ytick', 0.5:1:9.5, 'yticklabel', 9:-1:0);
+                spsMax(bnd) = max(get(sps(bnd), 'clim'));
                 title(bands{bnd});
+                sppPlotNorm = sppPlot./phasePowerNorm{bnd};
+                spsNorm(bnd) = subplot(2,length(bands),bnd+length(bands));
+                imagesc(-pi:0.1:(pi-0.1), 0:0.5:9.5, flipud(sppPlotNorm'));
+                set(gca, 'ytick', 0.5:1:9.5, 'yticklabel', 9:-1:0);
+%                 spsNormMax(bnd) = max(get(spsNorm(bnd), 'clim'));
+                spsNormMax(bnd) = mean(sppPlotNorm(:));
+                title([bands{bnd} ' Norm']);
                 drawnow;                
             end
+            spsLims = [0 max(spsMax)];
+            spsNormLims = [0 max(spsNormMax)*2];
+%             spsNormLims = [0 0.01];
+            for bnd = 1:length(bands)
+                set(sps(bnd), 'clim', spsLims);
+                set(spsNorm(bnd), 'clim', spsNormLims);
+            end
+            colorbar(sps(end), 'position', [0.9177, 0.5836, 0.0091, 0.3416]);
+            colorbar(spsNorm(end), 'position', [0.9177, 0.11, 0.0091, 0.3416]);
             if saveYN==1
                 set(gcf, 'PaperOrientation', 'landscape');
                 print('-fillpage', gcf, '-dpdf', sprintf('%s Spike-Phase-Power Plots', statMatrixColIDs{uniSpots(u)}));
