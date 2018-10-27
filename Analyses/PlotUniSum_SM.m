a38ac707f046d920ada1c9c31b0c79a29612e8c5
gaelias <gelias@uci.edu>
2018-10-26 17:30:27 -0700

    Update Analyses/PlotUniSum_SM.m

--- Analyses/PlotUniSum_SM.m
+++ Analyses/PlotUniSum_SM.m
@@ -28,12 +28,14 @@ if sum(uniSumLog) == 0
 end
 
 uniSumFiles = matFiles(uniSumLog);
+%% Declare runtime variables
+newCritF = 0.05/23;
 %% Run Analysis
 for u = 1:length(uniSumFiles)
     load(uniSumFiles{u});
     
     %% Plot Waveform Template
-    wire1 = axes('Position', [0.05 0.9 0.0325 0.05]);
+    wire1 = axes('Position', [0.05 0.85 0.0325 0.05]);
     xVals = 1:length(uniSum.TemplateMean{1});
     plot(wire1,xVals,uniSum.TemplateMean{1}, 'linewidth', 1.5, 'color', 'black');
     hold on;
@@ -43,7 +45,7 @@ for u = 1:length(uniSumFiles)
 %     title({[uniSum.UnitName]; 'Wire1'});
 %     title('Wire1')
     
-    wire2 = axes('Position', [0.1 0.9 0.0325 0.05]);
+    wire2 = axes('Position', [0.125 0.85 0.0325 0.05]);
     plot(wire2,xVals,uniSum.TemplateMean{2}, 'linewidth', 1.5, 'color', 'black');
     hold on;
     plot(wire2,xVals,uniSum.TemplateMean{2}+uniSum.TemplateStDev{2}, 'linewidth', 1, 'color', 'black', 'linestyle', ':');
@@ -52,7 +54,7 @@ for u = 1:length(uniSumFiles)
 %     title({[uniSum.UnitName]; 'Wire2'});
 %     title('Wire2')
     
-    wire3 = axes('Position', [0.15 0.9 0.0325 0.05]);
+    wire3 = axes('Position', [0.175 0.85 0.0325 0.05]);
     plot(wire3,xVals,uniSum.TemplateMean{3}, 'linewidth', 1.5, 'color', 'black');
     hold on;
     plot(wire3,xVals,uniSum.TemplateMean{3}+uniSum.TemplateStDev{3}, 'linewidth', 1, 'color', 'black', 'linestyle', ':');
@@ -61,7 +63,7 @@ for u = 1:length(uniSumFiles)
 %     title({[uniSum.UnitName]; 'Wire3'});
 %     title('Wire3')
     
-    wire4 = axes('Position', [0.2 0.9 0.0325 0.05]);
+    wire4 = axes('Position', [0.225 0.85 0.0325 0.05]);
     plot(wire4,xVals,uniSum.TemplateMean{4}, 'linewidth', 1.5, 'color', 'black');
     hold on;
     plot(wire4,xVals,uniSum.TemplateMean{4}+uniSum.TemplateStDev{4}, 'linewidth', 1, 'color', 'black', 'linestyle', ':');
@@ -90,7 +92,7 @@ for u = 1:length(uniSumFiles)
         {'\fontsize{10} Peak', '\downarrow'}, 'horizontalalignment', 'center', 'verticalalignment', 'bottom', 'parent', maxTemplate);
     
     %% Plot Mean Evoked Responses
-    meanFR = axes('Position', [0.05 0.68 0.135 0.15]);
+    meanFR = axes('Position', [0.05 0.6 0.135 0.15]);
     BarPlotErrorbars([uniSum.TrialEpochFRs.PreTrialFR(1), uniSum.TrialEpochFRs.EarlyTrialFR(1), uniSum.TrialEpochFRs.LateTrialFR(1), uniSum.TrialEpochFRs.PostTrialFR(1)],...
         [uniSum.TrialEpochFRs.PreTrialFR(2), uniSum.TrialEpochFRs.EarlyTrialFR(2), uniSum.TrialEpochFRs.LateTrialFR(2), uniSum.TrialEpochFRs.PostTrialFR(2)]);
     axis(meanFR, 'tight');
@@ -98,22 +100,61 @@ for u = 1:length(uniSumFiles)
     box off
     title('Mean Epoch Activity')
     
-    fdbkFR = axes('Position', [0.2 0.68 0.06 0.15]);
+    fdbkFR = axes('Position', [0.2 0.6 0.06 0.15]);
     BarPlotErrorbars([uniSum.TrialEpochFRs.RewardFR(1), uniSum.TrialEpochFRs.ErrorFR(1)],...
         [uniSum.TrialEpochFRs.RewardFR(2), uniSum.TrialEpochFRs.ErrorFR(2)]);
     axis(fdbkFR, 'tight');
     set(fdbkFR, 'xlim', [0 3], 'xtick', 1:2, 'xticklabel', {'Reward', 'Error'}, 'XTickLabelRotation', 45, 'color', 'none');
     box off
-    title('Feedback Activity')
-    
-    
+    title('Feedback Activity')  
     linkaxes([meanFR fdbkFR], 'y');
     
+    curYlim = get(meanFR, 'ylim');
+    set(meanFR, 'ylim', [curYlim(1) curYlim(2)+2]);
+    curYlim = get(meanFR, 'ylim');
+        
+    if uniSum.TrialEpochStats.TrialEpochsF(2)<newCritF
+        line(meanFR, [1 4], repmat(curYlim(2)-1, [1 2]), 'color', 'k');
+        text(meanFR,2.5, curYlim(2)-1+0.1, '\fontsize{20}\bf*');
+    end
+    if uniSum.TrialEpochStats.TrialPeriodsF(2)<newCritF
+        line(meanFR, [2 3], repmat(curYlim(2)-2, [1 2]), 'color', 'k');
+        text(meanFR, 2.5, curYlim(2)-2+0.1, '\fontsize{20}\bf*');
+    end
+    if uniSum.TrialEpochStats.FeedbackF(2)<newCritF
+        line(fdbkFR, [1 2], repmat(curYlim(2)-1, [1 2]), 'color', 'k');
+        text(fdbkFR, 1.5, curYlim(2)-1+0.1, '\fontsize{20}\bf*');
+    end
+        
+
     
-     
-     
-     
-     
+    %% Plot Correlation between epochs
+    rPcrit = 0.05/14;
+    curEpochCorrTable = uniSum.TrialEpochStats.EpochCorrelations.R;
+    curEpochSigTable = uniSum.TrialEpochStats.EpochCorrelations.P;
+    epochCorr = axes('position', [0.065 0.3 0.2 0.2]);
+    [r,c] = ind2sub(size(curEpochCorrTable), find(isnan(curEpochCorrTable)));
+    imagesc(curEpochCorrTable, [-1 1]);
+    hold on;
+    for p = 1:length(r)
+        patch([c(p)-0.5 c(p)-0.5 c(p)+0.5 c(p)+0.5], [r(p)-0.5 r(p)+0.5 r(p)+0.5 r(p)-0.5], 'white', 'edgecolor', 'white');
+    end
+    set(gca, 'xticklabels', {'Pre-Trial', 'Early-Trial', 'Late-Trial', 'Post-Trial', 'Reward', 'Error'}, 'XTickLabelRotation', 45,...
+        'yticklabels', {'Pre-Trial', 'Early-Trial', 'Late-Trial', 'Post-Trial', 'Reward', 'Error'});
+    hold on;
+    [col,row] = ind2sub([6 6],find(curEpochSigTable<rPcrit));
+    for roW = 1:length(row)
+        text(row, col, '*', 'horizontalalignment', 'center', 'fontweight', 'bold', 'fontsize', 20, 'color', 'white')
+    end
+    colormap jet
+    title('Trial Epoch Correlations');
+%     colorbar
+    cb = colorbar;
+    cb.Label.String = '\bf r-Val';
+    drawnow
     
+    %% 
+    uniSum.TrialEpochStats.PreTrial
     
-%% Summary 1: 
+
+    
