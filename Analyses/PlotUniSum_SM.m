54d829c0e751fef12c86c14940b7ff9601fbdb1b
gaelias <gelias@uci.edu>
2018-10-26 20:11:10 -0700

    Update Analyses/PlotUniSum_SM.m

--- Analyses/PlotUniSum_SM.m
+++ Analyses/PlotUniSum_SM.m
@@ -33,7 +33,11 @@ newCritF = 0.05/23;
 %% Run Analysis
 for u = 1:length(uniSumFiles)
     load(uniSumFiles{u});
-    
+    figure
+    %% Title
+    dirParts = strsplit(uniSum.Directory, '\');
+    figTitle = annotation('textbox', 'position', [0.025 0.935 0.7 0.05], 'String', ['\bf\fontsize{20}' sprintf('%s %s',uniSum.UnitName, dirParts{end})],...
+        'linestyle', 'none', 'horizontalalignment', 'left');
     %% Plot Waveform Template
     wire1 = axes('Position', [0.05 0.85 0.0325 0.05]);
     xVals = 1:length(uniSum.TemplateMean{1});
@@ -153,8 +157,157 @@ for u = 1:length(uniSumFiles)
     cb.Label.String = '\bf r-Val';
     drawnow
     
-    %% 
-    uniSum.TrialEpochStats.PreTrial
+    %% Plot Trial Epoch F-Values
+    preTrl = axes('position', [0.05 0.05 0.027 0.15]);    
+    bar(preTrl, 1:4, [uniSum.TrialEpochStats.PreTrial.PrevOdor(1),...
+        uniSum.TrialEpochStats.PreTrial.Odor(1),...
+        uniSum.TrialEpochStats.PreTrial.Position(1),...
+        uniSum.TrialEpochStats.PreTrial.NxtPos(1)]);    
+    box(preTrl, 'off');
+    set(preTrl, 'color', 'none', 'xlim', [0 5], 'xtick', 1:4,...
+        'xticklabel', {'PrvOdr', 'Odor', 'Pos', 'NxtPos'}, 'xticklabelrotation', 45);
+    title(preTrl, 'Pre-Trial')
+    if uniSum.TrialEpochStats.PreTrial.PrevOdor(2)<newCritF
+        text(preTrl,1, uniSum.TrialEpochStats.PreTrial.PrevOdor(1)+0.1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.PreTrial.Odor(2)<newCritF
+        text(preTrl,2, uniSum.TrialEpochStats.PreTrial.Odor(1)+0.1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.PreTrial.Position(2)<newCritF
+        text(preTrl,3, uniSum.TrialEpochStats.PreTrial.Position(1)+0.1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.PreTrial.NxtPos(2)<newCritF
+        text(preTrl,4, uniSum.TrialEpochStats.PreTrial.NxtPos(1)+0.1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
     
+    rlyTrl = axes('position', [0.09 0.05 0.027 0.15]);    
+    bar(rlyTrl, 1:4, [uniSum.TrialEpochStats.EarlyTrial.PrevOdor(1),...
+        uniSum.TrialEpochStats.EarlyTrial.Odor(1),...
+        uniSum.TrialEpochStats.EarlyTrial.Position(1),...
+        uniSum.TrialEpochStats.EarlyTrial.NxtPos(1)]);
+    box(rlyTrl, 'off');    
+    set(rlyTrl, 'color', 'none', 'xlim', [0 5], 'yticklabels', [], 'xtick', 1:4,...
+        'xticklabel', {'PrvOdr', 'Odor', 'Pos', 'NxtPos'}, 'xticklabelrotation', 45);
+    title(rlyTrl, 'Early-Trial')
+    if uniSum.TrialEpochStats.PreTrial.PrevOdor(2)<newCritF
+        text(rlyTrl,1, uniSum.TrialEpochStats.PrevOdor.PrevOdor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.EarlyTrial.Odor(2)<newCritF
+        text(rlyTrl,2, uniSum.TrialEpochStats.EarlyTrial.Odor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.EarlyTrial.Position(2)<newCritF
+        text(rlyTrl,3, uniSum.TrialEpochStats.EarlyTrial.Position(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.EarlyTrial.NxtPos(2)<newCritF
+        text(rlyTrl,4, uniSum.TrialEpochStats.EarlyTrial.NxtPos(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    
+    ltTrl = axes('position', [0.13 0.05 0.027 0.15]);    
+    bar(ltTrl, 1:4, [uniSum.TrialEpochStats.LateTrial.PrevOdor(1),...
+        uniSum.TrialEpochStats.LateTrial.Odor(1),...
+        uniSum.TrialEpochStats.LateTrial.Position(1),...
+        uniSum.TrialEpochStats.LateTrial.NxtPos(1)]);
+    box(ltTrl, 'off');
+    set(ltTrl, 'color', 'none', 'xlim', [0 5], 'yticklabels', [], 'xtick', 1:4,...
+        'xticklabel', {'PrvOdr', 'Odor', 'Pos', 'NxtPos'}, 'xticklabelrotation', 45);
+    title(ltTrl, 'Late-Trial')     
+    if uniSum.TrialEpochStats.LateTrial.PrevOdor(2)<newCritF
+        text(ltTrl,1, uniSum.TrialEpochStats.LateTrial.PrevOdor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.LateTrial.Odor(2)<newCritF
+        text(ltTrl,2, uniSum.TrialEpochStats.LateTrial.Odor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.LateTrial.Position(2)<newCritF
+        text(ltTrl,3, uniSum.TrialEpochStats.LateTrial.Position(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.LateTrial.NxtPos(2)<newCritF
+        text(ltTrl,4, uniSum.TrialEpochStats.LateTrial.NxtPos(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end   
+    
+    pstTrl = axes('position', [0.17 0.05 0.027 0.15]);    
+    bar(pstTrl, 1:4, [uniSum.TrialEpochStats.PostTrial.PrevOdor(1),...
+        uniSum.TrialEpochStats.PostTrial.Odor(1),...
+        uniSum.TrialEpochStats.PostTrial.Position(1),...
+        uniSum.TrialEpochStats.PostTrial.NxtPos(1)]);
+    box(pstTrl, 'off');
+    set(pstTrl, 'color', 'none', 'xlim', [0 5], 'yticklabels', [], 'xtick', 1:4,...
+        'xticklabel', {'PrvOdr', 'Odor', 'Pos', 'NxtPos'}, 'xticklabelrotation', 45);    
+    title(pstTrl, 'Post-Trial')         
+    if uniSum.TrialEpochStats.PostTrial.PrevOdor(2)<newCritF
+        text(pstTrl,1, uniSum.TrialEpochStats.PostTrial.PrevOdor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.PostTrial.Odor(2)<newCritF
+        text(pstTrl,2, uniSum.TrialEpochStats.PostTrial.Odor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.PostTrial.Position(2)<newCritF
+        text(pstTrl,3, uniSum.TrialEpochStats.PostTrial.Position(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.PostTrial.NxtPos(2)<newCritF
+        text(pstTrl,4, uniSum.TrialEpochStats.PostTrial.NxtPos(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end       
+    
+    rwd = axes('position', [0.21 0.05 0.027 0.15]);    
+    bar(rwd, 1:2, [uniSum.TrialEpochStats.Reward.Odor(1),...
+        uniSum.TrialEpochStats.Reward.Position(1)]);
+    box(rwd, 'off');
+    set(rwd, 'color', 'none', 'xlim', [0 3], 'yticklabels', [], 'xtick', 1:2,...
+        'xticklabel', {'Odor', 'Pos'}, 'xticklabelrotation', 45);            
+    title(rwd, 'Reward')               
+    if uniSum.TrialEpochStats.Reward.Odor(2)<newCritF
+        text(rwd,1, uniSum.TrialEpochStats.Reward.Odor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.Reward.Position(2)<newCritF
+        text(rwd,2, uniSum.TrialEpochStats.Reward.Position(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    
+    err = axes('position', [0.25 0.05 0.027 0.15]);    
+    bar(err, 1:2, [uniSum.TrialEpochStats.Error.Odor(1),...
+        uniSum.TrialEpochStats.Error.Position(1)]);
+    box(err, 'off'); 
+    set(err, 'color', 'none', 'xlim', [0 3], 'yticklabels', [], 'xtick', 1:2,...
+        'xticklabel', {'Odor', 'Pos'}, 'xticklabelrotation', 45);            
+    title(err, 'Error')                           
+    if uniSum.TrialEpochStats.Error.Odor(2)<newCritF
+        text(err,1, uniSum.TrialEpochStats.Error.Odor(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end
+    if uniSum.TrialEpochStats.Error.Position(2)<newCritF
+        text(err,2, uniSum.TrialEpochStats.Error.Position(1)+1, '\fontsize{15}\bf*', 'horizontalalignment', 'center');
+    end                   
+    
+    linkaxes([preTrl, rlyTrl, ltTrl, pstTrl, rwd, err], 'y');
+    
+    %% Plot Trial Rasters
+    trialTimeBins = uniSum.WholeTrial.TimeBins;
+    positionVals = uniSum.TrialInfo.Position;
+    posSortingMtx = [positionVals; 1:length(positionVals)]';
+    sortedPosMtx = sortrows(posSortingMtx);
+    
+    rasters = uniSum.WholeTrial.Rasters(sortedPosMtx(:,2))';
+    rasterTrl = cell(size(rasters));
+    rasterTrlPos = cell(size(rasters));
+    for r = 1:length(rasters)
+        rasterTrl{r} = repmat(r, size(rasters{r}));
+        rasterTrlPos{r} = repmat(sortedPosMtx(r,1), size(rasters{r}));
+    end
+    rasterScatVals = [cell2mat(rasterTrlPos), cell2mat(rasterTrl), cell2mat(rasters)];
+    
+    scatPlot = axes('Position', [0.3 0.7 0.65 0.25]);
+    hold on;
+    aLog = rasterScatVals(:,1)==1;
+    scatter(rasterScatVals(aLog,3), rasterScatVals(aLog,2), 10, [44/255 168/255 224/255], 'filled');
+    bLog = rasterScatVals(:,1)==2;
+    scatter(rasterScatVals(bLog,3), rasterScatVals(bLog,2), 10, [154/255 133/255 122/255], 'filled');
+    cLog = rasterScatVals(:,1)==3;
+    scatter(rasterScatVals(cLog,3), rasterScatVals(cLog,2), 10, [9/255 161/255 74/255], 'filled');
+    dLog = rasterScatVals(:,1)==3;
+    scatter(rasterScatVals(dLog,3), rasterScatVals(dLog,2), 10, [128/255 66/255 151/255], 'filled');
+    set(scatPlot, 'xlim', [min(trialTimeBins) max(trialTimeBins)], 'color', 'none');
+    line([0 0], get(scatPlot, 'ylim'), 'color','k');
+    axis(scatPlot, 'off');
+    
+    
+    
+        
+        
 
     
